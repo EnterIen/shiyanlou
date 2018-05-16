b037546b66e688fec241218368ac386be52218c7
entner <george_vibram@yeah.net>
2018-05-16 16:22:45 +0800

    seeker comment show

--- show.blade.php
+++ show.blade.php
@@ -5,68 +5,42 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">
-                <strong>{{ $question->title }}</strong>
-                </div>
+                <div class="card-header">评论</div>
 
-                <div class="card-body">
-
-                {{-- 标签 --}}
-                <p>
-                @foreach($question->labels as $label)
-                <a href="{{ route('label.show',[$label->id]) }}" class="badge badge-pill badge-primary">{{ $label->name }}</a>
+                <ul class="list-group list-group-flush">
+                @foreach($comments as $comment)
+                    <li class="list-group-item">
+                    <a href="{{ route('user.show',[$comment->user->id]) }}"><img class="avatar-small" src="{{ asset('images/avatar/'.$comment->user->avatar) }}">
+                    {{ $comment->user->name }}
+                    </a>
+                    @if($pid = $comment->parent_id)
+                    回复
+                    <a href="{{ route('user.show',[$pid]) }}">
+                    {{ App\User::where('id',$pid)->first()->name }}
+                    </a>
+                    @endif
+                    <p class="comment-content">{{ $comment->content }}
+                    <a class="btn-card-right btn-reply" href="javascript:void(0)" data-id="{{ $comment->user->id }}" data-name="{{ $comment->user->name }}">回复</a>
+                    </p>
+                    </li>
                 @endforeach
-                </p>
-                {{-- /标签 --}}
-
-                <p>{!! nl2br($question->description) !!}</p>
-
-
-                @if($question->user->id == Auth::id())
-                <a class="btn btn-link btn-card-right" href="{{ route('question.edit',[$question->id]) }}">编辑</a>
-                @endif
-
+                </ul>
+                <div class="card-body card-comment">
+                    <form method="post" action="{{ route('comment.store') }}">
+                      <input type='hidden' name='commentable_id' value="{{ $commentable_id }}">
+                      <input type='hidden' name='commentable_type' value="{{ $commentable_type }}">
+                      @csrf
+                      <div class="form-group">
+                        <input type="text" class="form-control" name="content" id="comment" value="{{old('content')}}" placeholder="添加评论">
+                        @if ($errors->has('content'))
+                        <p class="alert alert-danger">{{ $errors->first('content') }}</p>
+                        @endif
+                      </div>
+                      <button type="submit" class="btn btn-primary">提交</button>
+                    </form>
                 </div>
             </div>
-
-           @foreach($question->answers as $answer)
-           <div class="card" id="answer{{ $answer->id }}">
-           <div class="card-body">
-           <p><a href="{{ route('user.show',[$answer->user->id]) }}">
-           <img class="avatar-small" src="{{ asset('images/avatar/'.$answer->user->avatar) }}">
-           {{ $answer->user->name }}
-           </a></p>
-           <p>{!! nl2br($answer->content ?? '') !!}</p>
-           <input type="hidden" id="answer-content" value="{!! $answer->content ?? '' !!}">
-           <a href="{{ route('answer.like',[$answer->id]) }}" class="btn btn-warning">赞 {{ $answer->likes->count() }}</a>
-           <a class="btn btn-link" href="{{ route('answer.comment',[$answer->id]) }}">评论 {{ $answer->comments->count() }}</a>
-
-           @if ($answer->user->id == Auth::id())
-           <a href="#answer" class="btn btn-link btn-card-right" id="edit-answer">编辑</a>
-            @endif
-           </div>
-           </div>
-           @endforeach
-
-           <div class="card card-answer" style="{{ ($answer->user->id ?? 0) != Auth::id() ? '' : 'display:none' }}">
-           <div class="card-body">
-               <form method="post" action="{{ route('question.answer',[$question->id]) }}">
-                 @csrf
-                 <div class="form-group">
-                   <label for="question">添加回答</label>
-                   <textarea name="content" rows=8 class="form-control" id="answer" value="{{old('content')}}"></textarea>
-                    @if ($errors->has('content'))
-                    <p class="alert alert-danger">{{ $errors->first('content') }}</p>
-                    @endif
-                 </div>
-               <button type="submit" class="btn btn-primary">提交</button>
-               </form>
-           <div>
-           <div>
-
         </div>
     </div>
 </div>
 @endsection
-@endsection
-
